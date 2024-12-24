e123d40ef1af63abc2c7ef7693aaf46be72093e6
Nathan Arthur <nathan@nathanarthur.com>
2024-12-24 10:15:01 -0500

    add svelte view

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
-import { Plugin } from 'obsidian';
+import { Plugin, WorkspaceLeaf } from "obsidian";
+import { ExampleView, VIEW_TYPE_EXAMPLE } from "./ExampleView.js";
 
 export default class ResearchQuest extends Plugin {
   async onload() {
-    console.log('loading plugin')
+    console.log("loading plugin");
+
+    this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
+
+    this.addRibbonIcon("sparkles", "Activate view", () => {
+      this.activateView();
+    });
   }
   async onunload() {
-    console.log('unloading plugin')
+    console.log("unloading plugin");
+  }
+
+  async activateView() {
+    const { workspace } = this.app;
+
+    let leaf: WorkspaceLeaf | null = null;
+    const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
+
+    if (leaves.length > 0) {
+      // A leaf with our view already exists, use that
+      leaf = leaves[0];
+    } else {
+      // Our view could not be found in the workspace, create a new leaf
+      // in the right sidebar for it
+      leaf = workspace.getRightLeaf(false);
+      await leaf?.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
+    }
+
+    // "Reveal" the leaf in case it is in a collapsed sidebar
+    if (leaf) workspace.revealLeaf(leaf);
   }
 }
